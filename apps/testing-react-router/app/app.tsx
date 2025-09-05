618111aa4c526385eb0d021d2d8cffeab29d602f
Trong Lu <tronglu@192.168.5.165.non-exists.ptr.local>
2025-09-05 11:59:17 +0700

    first commit

---
+++ apps/testing-react-router/app/app.tsx
@@ -0,0 +1,13 @@
+import NxWelcome from "./nx-welcome";
+
+export function App() {
+  return (
+    <div>
+      <NxWelcome title="@testing-react-router-nx/testing-react-router"/>
+    </div>
+  );
+}
+
+export default App;
+
+
