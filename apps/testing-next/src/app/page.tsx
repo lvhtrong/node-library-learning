612403f9d6b64e7ba44ee4e97520eaede8b99df1
lvhtrong <35479106+lvhtrong@users.noreply.github.com>
2025-10-03 12:20:18 +0700

    fix: issue of build testing-next app (#12)

--- apps/testing-next/src/app/page.tsx
+++ apps/testing-next/src/app/page.tsx
@@ -3,14 +3,14 @@
 import { Admin } from '@library-testing/ui/react';
 
 export default function Index() {
+  const document = typeof window !== 'undefined' ? window.document : null;
+
   /*
    * Replace the elements below with your own.
    *
    * Note: The corresponding styles are in the ./index.tailwind file.
    */
   return (
-    <div className="h-screen">
-      <Admin></Admin>
-    </div>
+    <div className="h-screen">{document !== null && <Admin></Admin>}</div>
   );
 }
