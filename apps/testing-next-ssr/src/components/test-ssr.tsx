6c6c6de9551bc35a0b16684f94c176e8c629e40e
Trong Lu <lvhtrong1793@gmail.com>
2025-10-18 18:02:51 +0700

    fix build error

--- apps/testing-next-ssr/src/components/test-ssr.tsx
+++ apps/testing-next-ssr/src/components/test-ssr.tsx
@@ -1,7 +1,3 @@
 export const TestSSR = async () => {
-  const data = await fetch('http://localhost:3000/api/hello').then((res) =>
-    res.text()
-  );
-
-  return <div>Test SSR Component; data from /api/hello: {data}</div>;
+  return <div>Test SSR Component</div>;
 };
