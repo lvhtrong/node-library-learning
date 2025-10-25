d44fa434deb8d238a88c5f3b1c261f060dac7597
lvhtrong <35479106+lvhtrong@users.noreply.github.com>
2025-10-25 18:31:51 +0700

    feat: add testing-vue and testing-nuxt app (#14)
    * create testing-vue app
    
    * add testing-nuxt app
    
    * fix nuxt issue
    
    * update vitest config

---
+++ apps/testing-vue/src/views/HomeView.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import NxWelcome from '../app/NxWelcome.vue';
+</script>
+
+<template>
+  <main>
+    <NxWelcome title="@testing-react-router-nx/testing-vue" />
+  </main>
+</template>
